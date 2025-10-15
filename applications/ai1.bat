6a677ed53901b92a5c24e9307eec51f2c797db3a
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-15 12:42:28 +0800

    Add files via upload

---
+++ applications/ai1.bat
@@ -0,0 +1,9 @@
+call activate C://ai1
+cd %~dp0
+rem idk why echo is off after conda activate
+@echo on
+rem idk why chcp 65001 need run after conda activate, or it will activate failed
+chcp 65001
+set USE_LIBUV=0
+set KMP_DUPLICATE_LIB_OK=TRUE
+cmd
