cbef3058f58a9a9803e2bcdc915f3cccd3f056e9
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-16 16:20:28 +0800

    Add files via upload

---
+++ applications/debug_test_p.bat
@@ -0,0 +1,12 @@
+call activate C://ai1
+cd %~dp0
+rem idk why echo is off after conda activate
+@echo on
+rem idk why chcp 65001 need run after conda activate, or it will activate failed
+chcp 65001
+set USE_LIBUV=0
+set KMP_DUPLICATE_LIB_OK=TRUE
+
+python vmdlifting_p.py ..\data\images\test_image.png
+
+cmd
